b63e31dacc09334e3b3a73187a84c7ebdf732931
jasb-to <jaspalbilkhu@gmail.com>
2025-04-30 23:21:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ temperature, vibration, pressure, current)
 
 # Hugging Face API setup
 API_URL = "https://api-inference.huggingface.co/models/jbrownlee/Dummy-Predictive-Maintenance"
-headers = {"Authorization": "Bearer YOUR_HF_TOKEN"}  # ← Paste your token here
+headers = {"Authorization": "Bearer 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"}  # ← Paste your token here
 
 def predict(data):
     response = requests.post(API_URL, headers=headers, json={"inputs": data.to_dict()})
